7eeeb71f873f4649faab0f98a4fe6d5a17c402c9
Beni <benik@live.ca>
2022-11-23 01:10:16 -0500

    setup lessons

--- courses/urls.py
+++ courses/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import CourseListView,CourseDetailView
+from .views import CourseListView,CourseDetailView,LessonDetailView
 
 
 app_name = "courses"
@@ -23,5 +23,6 @@ app_name = "courses"
 urlpatterns = [
     path('', CourseListView.as_view(), name='list'),
     path('<slug>/', CourseDetailView.as_view(), name='detail'),
+    path('<course_slug>/<lesson_slug>/', LessonDetailView.as_view(),name='lesson-detail'),
 ]
 
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,View
 from .models import Course
 
 
@@ -15,5 +15,28 @@ class CourseDetailView(DetailView):
     model = Course
     
     context_object_name="course"
+
+
+class LessonDetailView(View):
+    
+    def get(self,request,course_slug,lesson_slug,*args, **kwargs):
+
+        course_qs = Course.objects.filter(slug=course_slug)
+        if course_qs.exists():
+            course = course_qs.first()
+
+
+        lesson_qs = course.lessons.filter(slug=lesson_slug)
+        if lesson_qs.exists():
+            lesson = lesson_qs.first()
+
+        context = {
+            'lesson':lesson
+
+        }
+
+        return render(request,"courses/lesson_detail.html",context)
+
+
 
 
